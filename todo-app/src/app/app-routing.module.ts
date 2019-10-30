54744621cea31e5b0a7e8eafb12e7f8f6bf7daa2
Dejiang Chen <dejiang27@gmail.com>
2019-10-29 19:21:54 -0500

    Updated routes

--- todo-app/src/app/app-routing.module.ts
+++ todo-app/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path:'', redirectTo:'/dashboard', pathMatch:'full'},
+  { path:'dashboard', component:DashboardComponent },
+  { path:'**', component:PageNotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
